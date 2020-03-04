48ca5e406dece4c711937ef0f0e5a84aa758ad02
1024735339@qq.com <1024735339@qq.com>
2020-03-04 22:51:49 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-electron-vue-flight'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
